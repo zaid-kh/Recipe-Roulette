1e4d7b0da30ec37e2e679c594e91e79729adcc41
zaid-kh <z.kh2800@gmail.com>
2024-01-16 11:08:35 +0200

    chore: added crud user routes

---
+++ server/routes/user.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+const router = Router();
+
+// GET /api/v1/users: Retrieve a list of all users.
+router.get("/", getAllUsers);
+
+// GET /api/v1/users/:id: Retrieve details of a specific user.
+router.get("/:id", getUserById);
+
+// POST /api/users: Add a new user to the database (admin-only).
+// Requires authentication and admin privileges.
+router.post("/", isAdmin, createUser);
+
+// PUT /api/users/:id: Update details of a specific user (admin-only).
+// Requires authentication.
+router.put("/:id", isAuth, updateUser);
+
+// DELETE /api/users/:id: Delete a user from the database (admin-only).
+// Requires authentication and admin privileges.
+router.delete("/:id", isAdmin, deleteUser);
+
+export default router;
