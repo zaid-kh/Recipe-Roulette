f2b02df8feeedb8bd69017956012254c0adabec1
zaid-kh <z.kh2800@gmail.com>
2024-01-20 14:03:14 +0200

    feat: Implemented base navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import theme from "./config/theme";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <BrowserRouter>
+          <Navbar />
           <Routes></Routes>
         </BrowserRouter>
       </ThemeProvider>
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Button,
+  useMediaQuery,
+  Box,
+} from "@mui/material";
+import { styled, useTheme } from "@mui/system";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link, useNavigate } from "react-router-dom";
+import theme from "../config/theme";
+
+const DrawerContainer = styled("div")(({ theme }) => ({
+  width: 240,
+  [theme.breakpoints.up("md")]: {
+    display: "none",
+  },
+}));
+
+const NavbarContainer = styled("div")(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}));
+
+const Navbar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const navigate = useNavigate();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const navLinks = [
+    { label: "Home", to: "/" },
+
+    { label: "Suggest", to: "/suggest" },
+    { label: "Sign In", to: "/signin" },
+  ];
+  // signedin navlinks
+  const signedInNavlinks = [
+    { label: "Home", to: "/" },
+    { label: "Suggest", to: "/suggest" },
+    { label: "Sign Out", to: "/signout" },
+  ];
+  let links = navLinks;
+  //   auth.currentUser ? (links = signedInNavlinks) : (links = navLinks);
+
+  const renderNavLinks = () => (
+    <List>
+      {links.map((item) => (
+        <ListItem key={item.label} disablePadding>
+          <ListItemButton component={Link} to={item.to}>
+            <ListItemText primary={item.label} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Recipe Roulette
+          </Typography>
+          {isMobile && (
+            <>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1, textAlign: "center" }}
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                Recipe Roulette
+              </Typography>
+              {/* //todo: could add logo */}
+              {/* <div>
+                <img
+                  src="your-logo.png"
+                  alt="Logo"
+                  style={{ width: "50px", height: "auto" }}
+                />
+              </div> */}
+            </>
+          )}
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {links.map((item) => (
+              <Button
+                key={item.label}
+                sx={{ color: theme.palette.text.secondary }}
+                component={Link}
+                to={item.to}
+              >
+                {item.label}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {(isMobile || !mobileOpen) && (
+        <DrawerContainer>
+          {/* Mobile Drawer */}
+          <Drawer
+            variant="temporary"
+            anchor="left"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {renderNavLinks()}
+          </Drawer>
+        </DrawerContainer>
+      )}
+      <NavbarContainer>
+        {/* Desktop Navigation */}
+        {isMobile || ""}
+      </NavbarContainer>
+    </div>
+  );
+};
+
+export default Navbar;
