c1171b9e733af2219bbe4ae29e25b415c3e5ac59
zaid-kh <z.kh2800@gmail.com>
2024-01-24 09:51:38 +0200

    feat: added modal to view steps

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -9,6 +9,8 @@ import {
   Box,
   Chip,
   Snackbar,
+  Modal,
+  Container,
 } from "@mui/material";
 import { Favorite, Save as SaveIcon } from "@mui/icons-material";
 import theme from "../config/theme";
@@ -17,20 +19,23 @@ const RecipeCard = ({ recipe, showSaveButton = false, onSaveClick }) => {
   const [snackbar, setSnackbar] = React.useState({
     open: false,
   });
-  const [modal, setModal] = React.useState({
-    open: false,
-  });
+  const [modalOpen, setModalOpen] = useState(false);
+  // favorite button handlers
   const handleSaveClick = () => {
     // todo: save recipe to user's favorites
     // onSaveClick(recipe._id);
     setSnackbar({ open: true });
   };
-  const handleClose = () => {
+  const handleCloseSnackbar = () => {
     setSnackbar({ open: false });
   };
+  // modal handlers
   const showSteps = () => {
     // show steps modal
-    setModal({ open: true });
+    setModalOpen(true);
+  };
+  const handleCloseModal = () => {
+    setModalOpen(false);
   };
 
   return (
@@ -71,6 +76,42 @@ const RecipeCard = ({ recipe, showSaveButton = false, onSaveClick }) => {
             />
           ))}
         </Box>
+        <Modal
+          open={modalOpen}
+          onClose={handleCloseModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: 400,
+              bgcolor: "background.default",
+              border: "2px solid #000",
+              boxShadow: 24,
+              p: 4,
+            }}
+          >
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Steps for {recipe.name}:
+            </Typography>
+            <Container id="modal-modal-description" sx={{ mt: 2 }}>
+              {recipe.steps.split(".").map((step, index) => {
+                if (index === recipe.steps.split(".").length - 1) {
+                  return null; // Skip the last step
+                }
+                return (
+                  <Typography key={index} variant="body2">
+                    {index + 1}. {step}
+                  </Typography>
+                );
+              })}
+            </Container>
+          </Box>
+        </Modal>
         {/* Display Dietary Options as Chips */}
         {/* <Box mt={2} display="flex" flexWrap="wrap">
           {recipe.dietaryOptions.map((option, index) => (
@@ -93,7 +134,7 @@ const RecipeCard = ({ recipe, showSaveButton = false, onSaveClick }) => {
             <Snackbar
               anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
               open={snackbar.open}
-              onClose={handleClose}
+              onClose={handleCloseSnackbar}
               message="Added to favorites!"
             />
             <Favorite />
